5af7e1b440eabf616759a5d63d4e99bbbc7fbdcf
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-11-30 13:46:23 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setup(
+    name="smg-imagesources",
+    version="0.0.1",
+    author="Stuart Golodetz",
+    author_email="stuart.golodetz@cs.ox.ac.uk",
+    description="A unified interface for image sources",
+    long_description="",  #long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sgolodetz/smg-imagesources",
+    packages=find_packages(include=["smg.imagesources", "smg.imagesources.*"]),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "smg-openni",
+        "smg-rotory"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
---
+++ smg/imagesources/__init__.py
@@ -0,0 +1,6 @@
+from .rgb_image_source import RGBImageSource
+from .rgbd_image_source import RGBDImageSource
+
+from .rgb_drone_camera import RGBDroneCamera
+from .rgb_from_rgbd_image_source import RGBFromRGBDImageSource
+from .rgbd_openni_camera import RGBDOpenNICamera
